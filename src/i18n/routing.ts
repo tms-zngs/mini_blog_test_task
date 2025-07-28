590f10386e9ff42a5f02489a73101f7e65dde736
Tomas Zingis <thebash2344@gmail.com>
2025-07-28 10:21:23 +0200

    clean up the code

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,9 +1,6 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
   locales: ["en", "uk"],
-
-  // Used when no locale matches
   defaultLocale: "en",
 });
